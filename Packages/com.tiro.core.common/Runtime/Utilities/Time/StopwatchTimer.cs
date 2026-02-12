82173113303d74e616226a89118fd0192a77f07c
Tarık Bulut <tbulut@sabanciuniv.edu>
2026-02-12 15:37:38 +0300

    added initial core common package

---
+++ Packages/com.tiro.core.common/Runtime/Utilities/Time/StopwatchTimer.cs
@@ -0,0 +1,15 @@
+namespace Tiro.Core.Common.Utilities.Time;
+
+public class StopwatchTimer : Timer {
+    public StopwatchTimer() : base(0) { }
+
+    public override void Tick(float deltaTime) {
+        if (IsRunning) {
+            Time += deltaTime;
+        }
+    }
+    
+    public void Reset() => Time = 0;
+    
+    public float GetTime() => Time;
+}
